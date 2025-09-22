1d5bc8c2872247bb440c9ff4efd7177af14eb006
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-09-22 21:51:06 +0800

    Solve the recursive Fibonacci sequence

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -12,4 +12,23 @@ function fibs(number) {
     return fibs;
 }
 
-console.log(fibs(8));
+// console.log(fibs(8));
+
+function fibsRec(number) {
+    console.log("This was printed recursively");
+    if (number === 1) return [0];
+
+    if (number === 2) {
+        return [0, 1];
+    }
+
+    let recur = fibsRec(number - 1);
+
+    const prev = recur[recur.length - 1];
+    const next = recur[recur.length - 2];
+    recur.push(prev + next);
+
+    return recur;
+}
+
+// console.log(fibsRec(8));
