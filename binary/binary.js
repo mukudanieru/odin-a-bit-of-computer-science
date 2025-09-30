947bd5e809865efecc9cab859a1f4408fddec434
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-09-30 20:55:40 +0800

    Move clean, and add constructTree and prettyPrint - Removed clean module (I realized that I don't actually need that LOL) - Moved deduplication and sorting logic from clean module to buildTree - Implemented recursive constructTree method for tree construction - Added prettyPrint function for better tree visualization

--- binary/binary.js
+++ binary/binary.js
@@ -1,4 +1,15 @@
-import { clean } from "./clean.js";
+function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+}
 
 class Node {
     constructor(value) {
@@ -12,7 +23,37 @@ class Tree {
     constructor() {
         this.root = null;
     }
+
+    buildTree(array) {
+        this.root = this.constructTree(
+            array
+                .filter((value, index, originalArray) => {
+                    return originalArray.indexOf(value) === index;
+                })
+                .sort((a, b) => a - b)
+        );
+
+        return this.root;
+    }
+
+    constructTree(arr) {
+        const low = 0,
+            high = arr.length;
+
+        if (arr.length === 0) return null;
+
+        const mid = low + Math.floor((high - low) / 2);
+
+        const node = new Node(arr[mid]);
+        node.left = this.constructTree(arr.slice(low, mid));
+        node.right = this.constructTree(arr.slice(mid + 1, high));
+
+        return node;
+    }
 }
 
-const arr = clean([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(arr);
+// const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const arr = [7, 6, 5, 4, 3, 2, 1];
+
+const tree = new Tree();
+prettyPrint(tree.buildTree(arr));
--- binary/clean.js
+++
@@ -1,7 +0,0 @@
-export function clean(arr) {
-    return arr
-        .filter((value, index, originalArray) => {
-            return originalArray.indexOf(value) === index;
-        })
-        .sort((a, b) => a - b);
-}
